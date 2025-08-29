b3cd3b877eb7d17444f1c55f84956c75539c5d5c
Shubham Verma <shubham.verma@innodeed.com>
2025-08-29 15:19:18 +0530

    Added Error and 404 Handling

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 
-// First Middleware (Logger) Log the request method and url
+// *First Middleware (Logger) Log the request method and url
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`);
     next();
 });
 
+// *Routes
 app.get('/', (req, res) => {
     res.send('Hello Shubham');
 });
@@ -24,7 +25,21 @@ app.get('/contact/:name', (req, res) => {
     res.send(`Hello ${name}`);
 });
 
-// Port Number
+// *Port Number
 app.listen(3000, () => {
     console.log('We have Started our server on port 3000');
+});
+
+
+// *404 Handler and Error Handler
+
+// *404 Handler
+app.use((req, res, next) => {
+    res.status(404).send('Route not found');
+});
+
+// *Error Handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
 });
